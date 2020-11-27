a543d7146334260db5347c82afb20597889ed3d9
Mark McLaughlin <mmclaugh@redhat.com>
2020-11-27 12:47:29 +0000

    Initial commit.

---
+++ src/test/java/org/donkey/app/NativeApiHitterIT.java
@@ -0,0 +1,9 @@
+package org.donkey.app;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeApiHitterIT extends ApiHitterTest {
+
+    // Execute the same tests but in native mode.
+}
