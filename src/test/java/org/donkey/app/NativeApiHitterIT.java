f1b60efb54a1e737fb31277fbaeb298b7ccdef39
Mark McLaughlin <mmclaugh@redhat.com>
2020-11-30 08:56:37 +0000

    Removed some junk.

--- src/test/java/org/donkey/app/NativeApiHitterIT.java
+++
@@ -1,9 +0,0 @@
-package org.donkey.app;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeApiHitterIT extends ApiHitterTest {
-
-    // Execute the same tests but in native mode.
-}
