4fc866d7f4981c1c94391edd64fb333aca60507b
Mark McLaughlin <mmclaugh@redhat.com>
2020-11-30 11:31:24 +0000

    Fixed reflection issue for native mode.

--- src/main/java/org/donkey/app/ExpirationTimestampPatch.java
+++ src/main/java/org/donkey/app/ExpirationTimestampPatch.java
@@ -1,9 +1,12 @@
 package org.donkey.app;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 import javax.json.bind.annotation.JsonbProperty;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
+@RegisterForReflection
 public class ExpirationTimestampPatch {
     @JsonbProperty("expiration_timestamp")
     private String expirationTimestamp;
